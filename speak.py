8a593607e5a50bba2e8911738fe7b2826e88f9ef
masakomaeda <masakomaeda126@yahoo.co.jp>
2020-10-02 08:05:15 +0900

    juliuconfを自作した

--- speak.py
+++ speak.py
@@ -1,12 +1,12 @@
-#!/usr/bin python3
+#!/usr/bin/env python3
 import pexpect
-import request
+import requests
 import os
 
 homeAddr = os.environ['HOME']
 
 p = pexpect.spawn("/bin/bash")
-p.sendline(f"julius -C {homeAddr}/WendyProject/myDict/wendy-speak.conf -demo")
+p.sendline(f"julius -C {homeAddr}/WendyProject/wendydict/wendystart.conf -demo")
 #p.sendline("julius -C ~/julius/dictation-kit/main.jconf -C ~/julius/dictation-kit/am-dnn.jconf -dnnconf ~/julius/dictation-kit/julius.dnnconf -demo")
 
 print("hello")
@@ -19,33 +19,39 @@ while True:
 
   output = str(p.before.decode())
   print(output)
-
+  url = ''
   if 'おはよう' in output:
-    print("sleepy")
-    url = 'http://localhost/io-api/templates/wings/action/sleepy'
-    response = requests.get(url)
+    print("sleepy->")
+    print(output)
+    print("sleepy<-")
+    url = 'http://localhost:8000/wings/action/sleepy'
   elif 'まさお' in output:
     print("delight")
-
-    url = 'http://localhost/io-api/templates/wings/action/delight/0'
-    response = requests.get(url)
+    url = 'http://localhost:8000/wings/action/delight/0'
   elif 'きつね' in output:
     print("delight")
-
-    url = 'http://localhost/io-api/templates/wings/action/delight/0'
-    response = requests.get(url)
-
+    url = 'http://localhost:8000/wings/action/delight/0'
+  elif 'すすめ' in output:
+    print("go")
+    url = 'http://localhost:8000/run/go'
   elif 'みぎ' in output:
     print("right")
+    url = 'http://localhost:8000/run/right'
   elif 'ひだり' in output:
     print("left")
+    url = 'http://localhost:8000/run/left'
   elif 'とまれ' in output:
-    print("stop")
-    
+    print("stop")  
+    url = 'http://localhost:8000/run/stop'
   elif 'さようなら' in output:
+    print("by!")
     break
+  else:
+    url = ''
+    pass
 
-    
-print("by!")
-p.terminate()
+  if 'http://' in url:
+    response = requests.get(url)  
+
+p.terminate()  
 p.expect(pexpect.EOF)
